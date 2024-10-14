97d83eaeb0e7963e3d672c96a64cbf809f773f70
Oleg Nechiporenko <root@TDK>
2024-10-14 21:51:39 +0300

    Fix types

--- tests/dummy/app/components/custom-toast.ts
+++ tests/dummy/app/components/custom-toast.ts
@@ -1,3 +1,5 @@
-import templateOnly from '@ember/component/template-only';
+import Component from '@glimmer/component';
+import type { ToastsBaseSignature } from 'ember-bootstrap-toasts-manager/components/toasts/base';
 
-export default templateOnly();
+// eslint-disable-next-line ember/no-empty-glimmer-component-classes
+export default class CustomToastComponent extends Component<ToastsBaseSignature> {}
